4d014bfe2dcc85b35499af7eb0c23c30609e48a5
KrisP-Sandoval <142380532+KrisP-Sandoval@users.noreply.github.com>
2024-03-03 21:24:49 +0800

    Delete StrategyPattern/StatePattern directory

--- StrategyPattern/StatePattern/AccountState.java
+++
@@ -1,9 +0,0 @@
-package StatePattern;
-
-public interface AccountState {
-    void deposit(double amount);
-    void withdraw(double amount);
-    void suspend();
-    void activate();
-    void close();
-}
